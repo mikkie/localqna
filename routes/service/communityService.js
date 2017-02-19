a5ed342a422ac7990b3e72dbecd5a784a730014d
benjaminl <benjamin.li@ehealth-china.com>
2017-02-19 13:43:29 +0800

    init

--- routes/service/communityService.js
+++ routes/service/communityService.js
@@ -1,10 +1,31 @@
 var communityDao = require('../dao/communityDao'),
-    conf = require('../conf/conf');
+    conf = require('../conf/conf'),
+    Q = require("q");
 
 
 var findTheNearByAndRecommendCommunities = function(loc,callback){
-    communityDao.findCommunitiesByDistance(loc,conf.settings.lbs.maxDistances,callback);
-    communityDao.findCommunitiesBySystemRecommend(callback);
+    var result = [];
+    Q.allSettled([findTheNearByCommunities(loc,result),findRecommendCommunities(result)]).then(function(result){
+        callback(result);
+    });
+};
+
+var findTheNearByCommunities = function(loc,result){
+    var deferred = Q.defer();
+    communityDao.findCommunitiesByDistance(loc,conf.settings.lbs.maxDistances,function(docs){
+        result.push(docs);
+        deferred.resolve();
+    });
+    return deferred.promise;
+};
+
+var findRecommendCommunities = function(result){
+    var deferred = Q.defer();
+    communityDao.findCommunitiesBySystemRecommend(function(docs){
+        result.push(docs);
+        deferred.resolve();
+    });
+    return deferred.promise;
 };
 
 
