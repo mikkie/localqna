aef4d04a9983e4dce283385616070863895f5fb8
mikkie <lmj3648xm@hotmail.com>
2017-04-03 10:54:02 +0800

    wx

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs= require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Linda' });
+});
+
+router.get('/fC9IXuyuXm.txt', function(req, res, next) {
+    fs.readFile(path.join(__dirname,'wx','fC9IXuyuXm.txt'),function(err,data){
+        res.writeHead(200,{'content-type':'text/plain'});
+        res.end(data.toString());
+    });
 });
 
 module.exports = router;
